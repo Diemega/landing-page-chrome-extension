65a69360e6110e6fc52449fcca265dbbc0ded1b2
Diemega <155173716+Diemega@users.noreply.github.com>
2024-09-13 05:12:58 +0000

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./**/*.html",
     "./**/*.js",
-    "./styles.css"
+    "./input.css"
   ],
   theme: {
     extend: {
@@ -11,8 +11,6 @@ module.exports = {
         'deep-purple': '#3A0CA3',
         'bright-blue': '#4CC9F0',
         'vibrant-pink': '#F72585',
-        'light-gray': '#F8F9FA',
-        'dark-blue-gray': '#2B2D42',
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
